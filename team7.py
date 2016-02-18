12e8d3e4dc8b6fd8c5c107ff2a243cb14a802fc4
Raj Manvar <rajmanvarchamp@gmail.com>
2016-02-18 15:08:13 +0530

    block_list function added

--- team7.py
+++ team7.py
@@ -5,12 +5,30 @@ first we find out which cells are valid '''
 class player7:
 
 
-        def get_block_list(old_move):
+        def get_block_list(old_move):  ''' Check the below code for blocks again  '''
 
             if old_move[0]==-1 and old_move[1]==-1: ''' we are getting first move so all blocks are valid '''
                 return range(9)
+            if old_move[0]%3==0 and old_move[1]%3==0 ''' top left cell of any block '''
+                return [1,3]
+            if old_move[0]%3==0 and old_move[1]%3==1 ''' top center cell of any block '''
+                return [0,2]
+            if old_move[0]%3==0 and old_move[1]%3==2 ''' top right cell of any block '''
+                return [1,5]
+            if old_move[0]%3==1 and old_move[1]%3==0 ''' center left cell of any block '''
+                return [0,6]
+            if old_move[0]%3==1 and old_move[1]%3==1 ''' center center cell of any block '''
+                return [4]
+            if old_move[0]%3==1 and old_move[1]%3==2 ''' center right cell of any block '''
+                return [2,8]
+            if old_move[0]%3==2 and old_move[1]%3==0 ''' bottom left cell of any block '''
+                return [3,7]
+            if old_move[0]%3==2 and old_move[1]%3==1 ''' bottom center cell of any block '''
+                return [6,8]
+            if old_move[0]%3==2 and old_move[1]%3==2 ''' bottom right cell of any block '''
+                return [5,7]
 
-            
+
 
     	def move(self, temp_board, temp_block, old_move, flag):
 
