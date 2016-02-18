faaf23bf2d2ecba319a1416624687208b53a46c3
Raj Manvar <rajmanvarchamp@gmail.com>
2016-02-18 15:35:13 +0530

    getting cell_list function added

--- team7.py
+++ team7.py
@@ -30,6 +30,33 @@ class player7:
 
 
 
+        def get_cell_list(allowed_block_list,temp_board,temp_block):
+
+            cell_list = []
+            for i in range(len(allowed_block_list)):
+                if temp_block[allowed_block_list[i]]!='-' : ''' the block is already won so we cannot move in that block'''
+                    continue ;
+
+                add_j = allowed_block_list[i]/3; '''  value to be added to first numbers from pairs  0,0 to 2,2 to get to block list'''
+                add_k = allowed_block_list[i]%3; '''  value to be added to second numbers from pairs  0,0 to 2,2 to get to block list'''
+                for j in range(3):
+                    for k in range(3):
+                        j_temp = j+add_j*3;
+                        k_temp = k+add_k*3;   ''' j and k will give all elements of a given block'''
+
+                        if(temp_board[j][k]=='-') :  ''' if the cell is empty '''
+                            cell_list.append( [j_temp,k_temp] );
+
+           if len(cell_list)==0 : ''' we have a free move '''
+            for j in len(8):
+                for k in len(8):
+                    if(temp_board[j][k]=='-') :  ''' if the cell is empty '''
+                        cell_list.append([j,k]);
+
+            return cell_list;
+
     	def move(self, temp_board, temp_block, old_move, flag):
 
             allowed_block_list = get_block_list(old_move);
+
+            get_cell_list(allowed_block_list,temp_board,temp_block)
