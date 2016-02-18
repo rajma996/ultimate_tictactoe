88a4c0e9cd22e769725b8f5060aa51bb10612921
Raj Manvar <rajmanvarchamp@gmail.com>
2016-02-18 14:54:25 +0530

    class and function set

--- team7.py
+++ team7.py
@@ -1,2 +1,17 @@
 ''' put everything in the class player7
 first we find out which cells are valid '''
+
+
+class player7:
+
+
+        def get_block_list(old_move):
+
+            if old_move[0]==-1 and old_move[1]==-1: ''' we are getting first move so all blocks are valid '''
+                return range(9)
+
+            
+
+    	def move(self, temp_board, temp_block, old_move, flag):
+
+            allowed_block_list = get_block_list(old_move);
